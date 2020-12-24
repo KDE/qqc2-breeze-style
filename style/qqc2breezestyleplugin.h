f09948126486c0619fa04a6fbb9a10ff293b9f9b
Noah Davis <noahadvs@gmail.com>
2020-12-24 09:36:28 -0500

    Remove an extra line in the plugin header license

--- style/qqc2breezestyleplugin.h
+++ style/qqc2breezestyleplugin.h
@@ -1,5 +1,4 @@
-/*
- * SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
+/* SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
  * SPDX-FileCopyrightText: 2020 Noah Davis <noahadvs@gmail.com>
  * SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later OR LicenseRef-KDE-Accepted-LGPL OR LicenseRef-KFQF-Accepted-GPL
  */
